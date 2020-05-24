7bf042466255b7cf146b54d7537b367da921be86
Tim Licata <tim@timothylicata.com>
2020-05-24 13:54:05 -0400

    Add online mode to help prevent runaway requests
    Add a online_flag macro that can be set at compile time with:
    
    c(oars, {d, online_flag}).

--- oars.erl
+++ oars.erl
@@ -2,6 +2,12 @@
 
 -export([street_list_url/1, street_url/2, from_url/1, run_services/0]).
 
+-ifdef(online_flag).
+-define(http_request(Url), httpc:request(Url)).
+-else.
+-define(http_request(Url), io:format("Offline mode. Not making request to: ~s~n", [Url])).
+-endif.
+
 base_url(City) ->
     io_lib:format("https://~s.oarsystem.com", [City]).
 
@@ -13,7 +19,8 @@ street_url(City, Street) ->
     io_lib:format("~s/assessment/pcllist.asp?~s", [base_url(City), Params]).
 
 from_url(Url) ->
-    {ok, Response} = httpc:request(Url),
+    {ok, Response} = ?http_request(Url),
+    io:format("ONLINE MODE! Request made to: ~s~n", [Url]),
     {{_Version, 200, _ReasonPhrase}, _Headers, Body} = Response,
     Body.
 
