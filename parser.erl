979f47196ef91a0a1237efad005ca523b7f6e2bb
Tim Licata <tim@timothylicata.com>
2020-05-24 14:12:04 -0400

    Add parser:just_one_space

--- parser.erl
+++ parser.erl
@@ -5,5 +5,6 @@
 no_breaks(Html) -> re:replace(Html, "<br />", "", [global, {return, list}]).
 no_newlines(Html) -> re:replace(Html, "\r\n", "", [global, {return, list}]).
 no_spaces(Html) -> re:replace(Html, " ", "", [global, {return, list}]).
+just_one_space(Html) -> re:replace(Html, "\s\s+", " ", [global, {return, list}]).
 
-clean(Html) -> no_breaks(no_newlines(Html)).
+clean(Html) -> just_one_space(no_breaks(no_newlines(Html))).
