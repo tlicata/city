831b3dafdf0b2630d1e52c6e76ae65aef28d91eb
Tim Licata <tim@timothylicata.com>
2020-12-04 22:00:56 -0500

    Better year parsing

--- address.erl
+++ address.erl
@@ -7,4 +7,7 @@ start(Address) ->
 
 init(Address) ->
     {Addr, _Url, Sbl, _LotSize, _PropType, _BuildingStyle, YearBuilt, _Sqft, _BedsBathsFire} = Address,
-    io:format("  My address is '~s', my Sbl is ~s, and I was built in ~p. (~p)~n", [Addr, Sbl, YearBuilt, self()]).
+    io:format("  My address is '~s', my Sbl is ~s, and I was built in ~p. (~p)~n", [Addr, Sbl, parse_year(YearBuilt), self()]).
+
+parse_year([]) -> 0;
+parse_year(Year) -> list_to_integer(Year).
