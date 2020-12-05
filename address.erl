b761ac8ef4de9f9ef9dda5729a198d8af312bbb1
Tim Licata <tim@timothylicata.com>
2020-12-04 22:26:10 -0500

    Let's make the address process loop

--- address.erl
+++ address.erl
@@ -7,7 +7,17 @@ start(City, Address) ->
 
 init(City, Address) ->
     {Addr, _Url, Sbl, _LotSize, _PropType, _BuildingStyle, YearBuilt, _Sqft, _BedsBathsFire} = Address,
-    io:format("  My address is '~s', my Sbl is ~s, and I was built in ~p. (~p)~n", [Addr, Sbl, parse_year(YearBuilt), self()]).
+    io:format("  My address is '~s', my Sbl is ~s, and I was built in ~p. Looping. (~p)~n", [Addr, Sbl, parse_year(YearBuilt), self()]),
+    loop(City, Address).
+
+loop(City, Address) ->
+    receive
+        url ->
+            io:format("I am an Address. My URL is ~s~n", [url(City, Address)]);
+        _Any ->
+            io:format("I am an Address. I don't understand that message~n")
+    end,
+    loop(City, Address).
 
 url(City, Address) ->
     oars:address_url(City, get_property_url(Address)).
