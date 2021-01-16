7bc59a0cf043bd553dc641985236042789f930de
Tim Licata <tim@timothylicata.com>
2021-01-16 17:14:27 -0500

    Change city_sup to call Joe Arm's area_server

--- apps/city/src/city_sup.erl
+++ apps/city/src/city_sup.erl
@@ -29,7 +29,7 @@ init([]) ->
     SupFlags = #{strategy => one_for_all,
                  intensity => 0,
                  period => 1},
-    ChildSpecs = [#{id => city, start => {city, start, []}}],
+    ChildSpecs = [#{id => city, start => {area_server, start_link, []}}],
     {ok, {SupFlags, ChildSpecs}}.
 
 %% internal functions
